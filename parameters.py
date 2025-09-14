39f2a8f4eb74189adf7a7abf36d79fcfd403683d
Alexandre Jonas <jonassouzafonseca@gmail.com>
2025-09-14 17:24:30 -0300

    Leitura de flags de operações

--- parameters.py
+++ parameters.py
@@ -11,6 +11,9 @@ def load_parameters():
     global debug_log_file_path
     global change_log_file_path
     global character_names_file_path
+    global is_hifen
+    global is_hifen_excecao
+    global is_change_name
 
     og_folder_path = os.getenv('og_folder_path')
     copy_folder_path = os.getenv('copy_folder_path')
@@ -19,5 +22,9 @@ def load_parameters():
     debug_log_file_path = os.getenv('debug_log_file_path')
     change_log_file_path = os.getenv('change_log_file_path')
     character_names_file_path = os.getenv('character_names_file_path')
+
+    is_hifen = ( os.getenv('is_hifen') == '1')
+    is_hifen_excecao = ( os.getenv('is_hifen_excecao') == '1')
+    is_change_name = ( os.getenv('is_change_name') == '1')
 
     return
